e44fd69cb73576655047142ad60ca2f67e804a4a
Stephen Serio <steph_stephen@outlook.com>
2022-02-21 13:42:47 -0600

    Added the links to the thumbnails of each project. Everything is still in great working order.

--- public/html-page.js
+++ public/html-page.js
@@ -1,3 +1,19 @@
+function toggleFold() {
+    let fold = document.getElementById('fold');
+    let content = fold.style.display;
+    let foldBtn = document.getElementById('foldBtn');
+
+    if (content == 'block') {
+        fold.style.display = 'none';
+        foldBtn.innerHTML = 'Show text'
+    } else {
+        fold.style.display = 'block';
+        foldBtn.innerHTML = 'Hide Text';
+    }
+}
+
+
+/*
 const fold = document.getElementsByClassName('contain')
 
 for (i = 0; i < fold.length; i++) {
@@ -5,3 +21,4 @@ for (i = 0; i < fold.length; i++) {
         this.classList.toggle('active')
     })
 }
+*/
